9a833033b343749e8a515ebadec7a19eec58ab10
Suraj0156 <130788586+Suraj0156@users.noreply.github.com>
2023-07-25 15:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const evaluatedText = document.getElementById('evaluatedText');
+  const letterCount = document.getElementById('letterCount');
+
+  // Function to update letter count in real-time
+  const updateLetterCount = () => {
+    const text = evaluatedText.value;
+    const count = text.length;
+    letterCount.textContent = count;
+  };
+
+  // Attach event listener to the input textbox
+  evaluatedText.addEventListener('input', updateLetterCount);
+
+  // Initialize letter count to 0 on page load
+  updateLetterCount();
+});
+
